79a714651572e1aa49a4ada17f8bbc0acbbbf3cf
matheus.toniotti <matheus.toniotti@gmail.com>
2020-03-06 15:31:00 -0300

    ééééé

--- src/main/java/br/com/hbsis/core/Main.java
+++ src/main/java/br/com/hbsis/core/Main.java
@@ -7,19 +7,25 @@ import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(5501);
-        while (true) {
-            String fileLocation = "/home/logs/log"+ UUID.randomUUID() +".txt";
-            Socket socket = serverSocket.accept();
-            File file = new File(fileLocation);
-            file.createNewFile();
-            byte[] bytes = new byte[16 * 1024];
-            try (OutputStream out = new FileOutputStream(file); InputStream in = socket.getInputStream()) {
-                int count;
-                while ((count = in.read(bytes)) > 0) {
-                    out.write(bytes, 0, count);
+        try {
+            ServerSocket serverSocket = new ServerSocket(5501);
+            while (true) {
+                String fileLocation = "/home/logs/log" + UUID.randomUUID() + ".txt";
+                Socket socket = serverSocket.accept();
+                File file = new File(fileLocation);
+                file.createNewFile();
+                byte[] bytes = new byte[16 * 1024];
+                try (OutputStream out = new FileOutputStream(file); InputStream in = socket.getInputStream()) {
+                    int count;
+                    while ((count = in.read(bytes)) > 0) {
+                        out.write(bytes, 0, count);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
