733bd6bbfbed86946bb7d19c46ba13e32d0deb29
matheus.toniotti <matheus.toniotti@gmail.com>
2020-03-09 13:27:03 -0300

    arrumando o path dos arquivos

--- src/main/java/br/com/hbsis/core/Main.java
+++ src/main/java/br/com/hbsis/core/Main.java
@@ -6,26 +6,33 @@ import java.net.Socket;
 import java.util.UUID;
 
 public class Main {
+    private static int numberThread = 0;
+
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(5501);
         while (true) {
-            Socket socket = serverSocket.accept();
-            String systemSeparator = System.getProperty("file.separator");
-            String filePath = System.getProperty("user.home")+systemSeparator+"logfiles"+systemSeparator;
-            String fileName = "log_" + UUID.randomUUID() + ".txt";
-            System.out.println(filePath+fileName);
-            new File(filePath).mkdir();
-            File file = new File(filePath+fileName);
-            byte[] bytes = new byte[16 * 1024];
-            try(OutputStream out = new FileOutputStream(file)){
-                InputStream in = socket.getInputStream();
-                int count;
-                while ((count = in.read(bytes)) > 0) {
-                    out.write(bytes, 0, count);
-                }
-            } catch (Exception e) {
-                continue;
+            if(numberThread <= 3){
+                Socket socket = serverSocket.accept();
+                new Thread(() -> {faz(socket);}).start();
+            }else{continue;}
+        }
+    }
+
+    private static void faz(Socket socket) {
+        String systemSeparator = System.getProperty("file.separator");
+        String filePath = System.getProperty("user.home") + systemSeparator + "logfiles" + systemSeparator;
+        String fileName = "log_" + UUID.randomUUID() + ".txt";
+        System.out.println(filePath + fileName);
+        new File(filePath).mkdir();
+        File file = new File(filePath + fileName);
+        byte[] bytes = new byte[16 * 1024];
+        try (OutputStream out = new FileOutputStream(file)) {
+            InputStream in = socket.getInputStream();
+            int count;
+            while ((count = in.read(bytes)) > 0) {
+                out.write(bytes, 0, count);
             }
+        } catch (Exception e) {
         }
     }
 }
